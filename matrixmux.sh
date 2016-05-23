e319f40593cb12056aae260f1a2b309f2bb6f0a0
Stacey Sharp <ssharpjr@gmail.com>
2016-05-23 11:13:42 -0400

    updated index on matrixmux

--- matrixmux.sh
+++ matrixmux.sh
@@ -1,8 +1,8 @@
 tmux new -s matrix -d 'cmatrix -a -u 1 -C green' \; \
 splitw -h -p 50 'cmatrix -a -u 1 -C blue' \; \
-selectp -t 1 \; \
+selectp -t 2 \; \
 splitw -h -p 50 'cmatrix -a -u 1 -C cyan' \; \
-selectp -t 0 \; \
+selectp -t 1 \; \
 splitw -h -p 50 'cmatrix -a -u 1 -C red' \; \
 attach -t matrix
 
